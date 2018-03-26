cd55ef6e0d2752e3bb482c441013e4c0179959f2
sonicdm <sonicdm@gmail.com>
2018-03-26 15:54:05 -0700

    Fixed some config issues

--- thought_bot/config.py
+++ thought_bot/config.py
@@ -3,5 +3,5 @@ bad_thoughts_path  = r"J:\TOOL KIT\scripts\Thought Bot\thought_bot\bad_thoughts.
 default_host_name = "Your Inner Voice"
 default_round_length = 5
 default_round_count = 1
-watson_api_username = '0966265a-b0c0-4b35-9f8b-a53e934eeb1a'
-watson_api_password = 'ukKiZ7JE2sbj'
+watson_api_username = ''
+watson_api_password = ''
