bd7465c7df475362d6d9c176832150a68ebf0a86
Allan Barcellos <sonicdm@gmail.com>
2018-04-03 10:38:54 -0700

    Update config.py

--- thought_bot/config.py
+++ thought_bot/config.py
@@ -3,5 +3,6 @@ bad_thoughts_path  = r"J:\TOOL KIT\scripts\Thought Bot\thought_bot\bad_thoughts.
 default_host_name = "Your Inner Voice"
 default_round_length = 5
 default_round_count = 1
+# sign up for a free watson AI api key https://www.ibm.com/watson/
 watson_api_username = ''
 watson_api_password = ''
